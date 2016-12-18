2ae5bd565f9a50d7e61fb3360dfa921d3a3e5415
Odzhan <odzhan@protonmail.com>
2016-12-18 06:04:25 +0000

    upd

--- bld.bat
+++ bld.bat
@@ -1,7 +1,5 @@
 @echo off
 del *.obj *.bin *.exe
-cl /Os /O2 /Fa /c /GS- belt.c
-jwasm -bin belt.asm
 yasm -fbin -DBIN bx.asm -obx.bin
 yasm -fwin32 bx.asm -obx.obj
 cl /nologo /DUSE_ASM /O2 /Os /GS- test.c bx.obj
